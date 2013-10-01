95332430bed08be76a3f5b8f53911e7342c099e5
Sanchit Bareja <sanchitbareja@gmail.com>
2013-09-30 21:45:29 -0700

    added fields to companies admin model

--- companies/admin.py
+++ companies/admin.py
@@ -4,6 +4,6 @@ from companies.models import Company
 #register the admin site
 
 class CompanyAdmin(admin.ModelAdmin):
-	list_display = ['id','name','founded','funding','website','careers_website','logo','banner_image','number_employees','organization_type','company_description','competitors','avg_salary']
+	list_display = ['id','name','founded','funding','website','careers_website','logo','banner_image','number_employees','organization_type','company_description','competitors','avg_salary','location','intro_video']
 
 admin.site.register(Company,CompanyAdmin)
