06a62c2875bf7801cec90f4e85f2f1b19d37815d
Sanchit Bareja <sanchitbareja@gmail.com>
2013-10-05 16:35:50 -0700

    implemented live search

--- api/api.py
+++ api/api.py
@@ -14,6 +14,7 @@ from django.conf.urls.defaults import *
 from django.core.paginator import Paginator, InvalidPage
 from tastypie.utils import trailing_slash
 from django.http import Http404
+from haystack.query import SearchQuerySet
 
 # misc
 from django.db.models import Q
@@ -68,6 +69,39 @@ class CompanyResource(ModelResource):
         filtering = {
             "name": ("exact")
         }
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/search%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_search'), name="api_get_search"),
+        ]
+
+    def get_search(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        self.is_authenticated(request)
+        self.throttle_check(request)
+
+        # Do the query.
+        sqs = SearchQuerySet().models(Company).load_all().auto_query(request.GET.get('q', ''))
+        paginator = Paginator(sqs, 20)
+
+        try:
+            page = paginator.page(int(request.GET.get('page', 1)))
+        except InvalidPage:
+            raise Http404("Sorry, no results on that page.")
+
+        objects = []
+
+        for result in page.object_list:
+            bundle = self.build_bundle(obj=result.object, request=request)
+            bundle = self.full_dehydrate(bundle)
+            objects.append(bundle)
+
+        object_list = {
+            'response': {'companies':objects},
+        }
+
+        self.log_throttled_access(request)
+        return self.create_response(request, object_list)
 
     def dehydrate(self, bundle):
         """
--- companies/search_indexes.py
+++ companies/search_indexes.py
@@ -6,11 +6,11 @@ from companies.models import Company
 class CompanyIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     name = indexes.CharField(model_attr='name')
-    website = indexes.CharField(model_attr='website')
-    organization_type = indexes.CharField(model_attr='organization_type')
-    company_description = indexes.CharField(model_attr='company_description')
-    competitors = indexes.CharField(model_attr='competitors')
-    location = indexes.CharField(model_attr='location')
+    website = indexes.CharField(model_attr='website', null=True)
+    organization_type = indexes.CharField(model_attr='organization_type', null=True)
+    company_description = indexes.CharField(model_attr='company_description', null=True)
+    competitors = indexes.CharField(model_attr='competitors', null=True)
+    location = indexes.CharField(model_attr='location', null=True)
 
     def get_model(self):
         return Company
