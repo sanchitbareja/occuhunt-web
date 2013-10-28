ddf4a816cd77079ec65e4999159dfda0bbe35d96
Sanchit Bareja <sanchitbareja@gmail.com>
2013-10-28 15:57:23 -0700

    there was an error where the linkedin login would only return a dict if there was only 1 education however it woudl return a list otherwise

--- api/pipeline.py
+++ api/pipeline.py
@@ -2,11 +2,19 @@ from django.contrib.auth.models import Group
 
 def linkedin_test(request, backend, *args, **kwargs):
     try:
-        for education in kwargs['response']['educations']['education']:
-            if education['school-name'] == "University of California, Berkeley":
-                g = Group.objects.get(name='UC Berkeley')
-                user = kwargs['user']
-                user.groups.add(g)
-                user.save()
+        print kwargs
+        if type(kwargs['response']['educations']['education']) == type({}):
+            if "University of California, Berkeley".lower() in kwargs['response']['educations']['education']['school-name'].lower():
+                    g = Group.objects.get(name='UC Berkeley')
+                    user = kwargs['user']
+                    user.groups.add(g)
+                    user.save()
+        elif type(kwargs['response']['educations']['education']) == type([]):
+            for education in kwargs['response']['educations']['education']:
+                if "University of California, Berkeley".lower() in education['school-name'].lower():
+                    g = Group.objects.get(name='UC Berkeley')
+                    user = kwargs['user']
+                    user.groups.add(g)
+                    user.save()
     except Exception as e:
         print e
