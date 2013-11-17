261e8cd5b855129ce1f6d5897eaf2a8e379d8683
Sanchit Bareja <sanchitbareja@gmail.com>
2013-11-17 11:02:17 -0800

    added loading spinner for pdf rendering

--- occuhunt/static/occuhunt/js/resume_feed.js
+++ occuhunt/static/occuhunt/js/resume_feed.js
@@ -62,6 +62,9 @@ function parse_pdf(data){
     // init variables
     pageStarts[0] = 0;
 
+    // Show loading state
+    $("#file_upload").before('<br /></br /><div id="spinnerWait"><div class="spinner"><div class="bar1"></div><div class="bar2"></div><div class="bar3"></div><div class="bar4"></div><div class="bar5"></div><div class="bar6"></div><div class="bar7"></div><div class="bar8"></div><div class="bar9"></div><div class="bar10"></div><div class="bar11"></div><div class="bar12"></div></div><p></p></div>');
+
     PDFJS.getDocument(data).then(function(pdf) {
         pdfPages = pdf;
         numPages = pdf.numPages;
