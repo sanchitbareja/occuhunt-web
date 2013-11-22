a467281dfb8099da82bf84012fe15179e422d617
Sanchit Bareja <sanchitbareja@gmail.com>
2013-11-22 11:38:09 -0800

    increased expiry time for signed request

--- resumes/views.py
+++ resumes/views.py
@@ -6,7 +6,7 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
-import os, time, simplejson, base64, urllib, hmac, sha, random, string
+import os, time, simplejson, base64, urllib2, hmac, sha, random, string
 from string import strip
 from django.http import Http404
 from django.core import serializers
@@ -53,21 +53,21 @@ def sign_s3_upload(request):
     object_name = request.GET['s3_object_name'] + "".join(lst) # creates a unique name 
     mime_type = request.GET['s3_object_type']
 
-    expires = int(time.time()+10)
+    expires = int(time.time()+400)
     amz_headers = "x-amz-acl:public-read"
 
     put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
 
     signature = base64.encodestring(hmac.new(AWS_SECRET_KEY,put_request, sha).digest())
-    signature = urllib.quote_plus(signature.strip())
+    signature = signature.strip()
 
     url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
     signed_request = '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature)
 
     json_results = simplejson.dumps({
         'signed_request': signed_request,
-         'url': strip(url)
-      })
+        'url': url
+    })
 
     return HttpResponse(json_results, mimetype='application/json')
 
