6f19192b4f3e31ee5fe039b9babe595e8efbb8fe
Sanchit Bareja <sanchitbareja@gmail.com>
2014-04-07 00:14:33 -0700

    fixed bugs for 357

--- fairs/views.py
+++ fairs/views.py
@@ -69,3 +69,20 @@ def PBLCareerFairSpring2014View(request):
 			resume = None
 			return render_to_response('CareerFairs/UCBerkeley/PBLCareerFairSpring2014.html', {'resume_url':False}, RequestContext(request))
 
+
+def Dropin357View(request):
+	"""View to ISchoolInfoCamp """
+	print request.user
+	if request.user.is_anonymous():
+		# if it's anonymous user
+		return render_to_response('CareerFairs/UCBerkeley/357April92014.html', {'resume_url':False}, RequestContext(request))
+	else:
+		resume = Resume.objects.filter(user=request.user, showcase=True, original=False).order_by('-timestamp')
+		if len(resume) > 0:
+			# if user has a resume - yay!
+			resume = resume[0]
+			return render_to_response('CareerFairs/UCBerkeley/357April92014.html', {'resume_url':resume.url}, RequestContext(request))
+		else:
+			# if user doesn't have a resume on file, boo :(
+			resume = None
+			return render_to_response('CareerFairs/UCBerkeley/357April92014.html', {'resume_url':False}, RequestContext(request))
